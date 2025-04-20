2c447c7b432594fa8372541ae38aecbd82b28129
Seongbin <90225185+kseongbin@users.noreply.github.com>
2025-04-21 00:04:52 +0900

    Create 2798.kt

---
+++ 2798.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+    val cards = readLine()!!.split(" ").map { it.toInt() }
+    var bestSum = 0
+
+    for (i in 0 until n - 2) {
+        for (j in i + 1 until n - 1) {
+            for (k in j + 1 until n) {
+                val sum = cards[i] + cards[j] + cards[k]
+                if (sum <= m && sum > bestSum) {
+                    bestSum = sum
+                }
+            }
+        }
+    }
+
+    println(bestSum)
+}
